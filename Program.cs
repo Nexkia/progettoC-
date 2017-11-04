09dcf20d97101d1796c8e36961f080e5b7a993a3
Nexkia <giuseppe794@yahoo.it>
2017-11-04 17:52:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ServiceAPI.Dal;
+using ServiceAPI.Entities;
+
+namespace SuperCoolApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new GameLibraryDbContext())
+            {
+                context.Database.EnsureCreated();
+            }
+            BuildWebHost(args).Run();
+
+        }
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
